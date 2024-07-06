1995e45d0d2e59886b5f079a1b88c303fc748118
Arya Rakha <33478892+arrakh@users.noreply.github.com>
2024-07-06 16:34:06 +0700

    init commit

---
+++ MultiEventHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Arr.EventsSystem
+{
+    public partial class EventHandler
+    {
+        public void RegisterMultiple(object listener)
+        {
+            var interfaces = listener.GetType().GetInterfaces();
+            foreach (var i in interfaces)
+            {
+                if (!i.IsGenericType) continue;
+
+                var genericType = i.GetGenericTypeDefinition();
+                var args = i.GetGenericArguments();
+                InvokeEventFunction(nameof(Register), listener, genericType, method => method.MakeGenericMethod(args));
+            }
+        }
+
+        private void InvokeEventFunction(string funcName, object listener, Type genericEventType, Func<MethodInfo, MethodInfo> makeGenericMethodFunc)
+        {
+            var method = typeof(EventHandler)
+                .GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                .FirstOrDefault(m => m.IsGenericMethod && m.Name == funcName
+                                                       && m.GetParameters().Length == 1
+                                                       && m.GetParameters()[0].ParameterType.GetGenericTypeDefinition() == genericEventType);
+                             
+            if (method == null)
+            {
+                #if ARR_SUPPRESS_EXCEPTIONS
+                throw new Exception($"Failed to find the Register method of type {genericEventType}.");
+                #endif
+                return;
+            }
+            
+            var genericMethod = makeGenericMethodFunc.Invoke(method);
+
+            genericMethod.Invoke(this, new object[] { listener });
+        }
+
+        public void UnregisterMultiple(object listener)
+        {
+            var interfaces = listener.GetType().GetInterfaces();
+            
+            foreach (var i in interfaces)
+            {
+                if (!i.IsGenericType) continue;
+
+                var genericType = i.GetGenericTypeDefinition();
+                var args = i.GetGenericArguments();
+                InvokeEventFunction(nameof(Unregister), listener, genericType, method => method.MakeGenericMethod(args));
+            }
+        }
+    }
+}
